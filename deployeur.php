5ebc36118b75cc709586281c1ba0003e56092368
Logan Blangenois <logan.blangenois@mediakod.com>
2022-11-30 09:48:42 +0100

    chore: add Loader

--- deployeur.php
+++ deployeur.php
@@ -10,5 +10,37 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Version:     0.1.0
  * Text Domain: mk-deployeur
- *
+ * Prefix: mkd_
+ * 
  */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+	die;
+}
+
+define('MKD_VERSION', '0.1.0');
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path(__FILE__) . 'includes/class-deployeur.php';
+
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_deployeur() {
+
+	$plugin = new Deployeur();
+	$plugin->run();
+}
+
+run_deployeur();
