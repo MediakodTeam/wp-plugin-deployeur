e993f78556ec56d9ebddd06e9caa35438b73ca38
Logan Blangenois <logan.blangenois@mediakod.com>
2022-11-30 09:05:55 +0100

    chore: init plugin

---
+++ deployeur.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: Deployeur
+ * Description: A WordPress plugin that use Webhooks to deploy and make history of your WordPress edition.
+ * Plugin URI:  https://mediakod.com
+ * Author:      Mediakod
+ * Author URI:  https://mediakod.com
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Version:     0.1.0
+ * Text Domain: mk-deployeur
+ *
+ */
