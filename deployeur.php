ea623cb22f634bbfd7dd9002b7cdee3d0099fe83
Logan Blangenois <logan.blangenois@mediakod.com>
2025-05-28 10:13:51 +0200

    prerelease: 1.1.5.canary-1

--- deployeur.php
+++ deployeur.php
@@ -8,7 +8,7 @@
  * Author URI:  https://mediakod.com
  * License:     GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Version:     1.1.4
+ * Version:     1.1.5.canary-1
  * Text Domain: mk-deployeur
  * Prefix: mkd_
  * 
@@ -19,7 +19,7 @@ if (!defined('WPINC')) {
 	die;
 }
 
-define('MKD_VERSION', '1.1.4');
+define('MKD_VERSION', '1.1.5.canary-1');
 
 /**
  * The code that runs during plugin activation.
